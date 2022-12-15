c54a600be9e5aeb061e60e9472779e6a2828ac5a
Rob Dorsett <robjakedorsett@gmail.com>
2022-12-15 09:47:18 +0000

    Initial Commit

---
+++ src/App_Plugins/AbsurdOpenAI/js/absurdOpenAi.controller.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco")
+    .controller("Absurd.OpenAI", function ($scope, $http) {
+
+        var vm = this
+        //set default states for flags
+        vm.clipboardSuccess = false;
+        vm.processing = false;
+
+        //call internal open AI endpoint
+        vm.search = function () {
+            vm.processing = true;
+            $http.get("/umbraco/backoffice/OpenAI/Completion/GetText?text=" + vm.value)
+                .then(function success(resp) {
+                    vm.resp = resp;
+                    vm.processing = false;
+                }, function error(resp) {
+                    vm.resp = resp.data.ExceptionMessage;
+                    vm.processing = false;
+                });
+        };
+
+        //copy to clipboard functionality
+        vm.copyClipboard = function () {
+            navigator.clipboard.writeText(vm.resp);
+            vm.clipboardSuccess = true;
+            setTimeout(() => vm.clipboardSuccess = false, 600);
+        };
+    });
---
+++ src/Controllers/CompletionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+using System.Threading.Tasks;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common.Attributes;
+
+namespace AbsurdOpenAI.Controllers
+{
+    [PluginController("OpenAI")]
+    public class CompletionController : UmbracoAuthorizedApiController
+    {
+        private readonly OpenAIAPI _client;
+
+        public CompletionController(IConfiguration configuration)
+        {
+            var apiKey = configuration.GetValue<string>("OpenAI:ApiKey");
+            _client = new OpenAIAPI(apiKey, Engine.Davinci);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetText(string text)
+        {
+            var request = new CompletionRequest(text)
+            {
+                MaxTokens = 256,
+                Temperature = 0.7,
+                TopP = 1,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0
+            };
+            var completion = await _client.Completions.CreateCompletionAsync(request);
+
+            return new JsonResult(completion.ToString());
+        }
+    }
+}
